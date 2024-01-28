0a248f4ad10ec215b7e7754f941c0cf388a8f553
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2024-01-28 16:56:53 -0500

    Fix: Adjust padding on register and logout prefunction is ready

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login');
+  }
   return (
     <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:justify-between py-10">
       <p className="text-white text-2xl font-bold">Smart Library<span className="text-[#AD6BF1]">.</span></p>
       
       <div className="flex gap-10 text-white bg-[#432b84] p-3 rounded-xl">
         <p className="font-medium">Hola, Username</p>
-        <FontAwesomeIcon className="text-2xl cursor-pointer" icon={faRightToBracket} />
+        <FontAwesomeIcon className="text-2xl cursor-pointer" icon={faRightToBracket} onClick={() => handleLogout()} />
       </div>
     </div>
   )
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,7 +3,7 @@ import backgroundImage from '../assets/backgroundauth.png';
 export const AuthLayout = ({children}) => {
     return (
         <div className="flex justify-center items-center min-h-screen min-w-screen absolute top-0 z-[-2] h-screen w-screen bg-cover bg-gradient-to-b from-sky-950 to-sky-100" style={{backgroundImage: `url(${backgroundImage})`}}>
-            <div className="shadow-md rounded-md max-[600px] flex flex-col justify-center p-10 md:px-20 md:py-14 mx-8 bg-[#181827]">
+            <div className="shadow-md rounded-md max-[600px] flex flex-col justify-center p-10 md:px-20 md:py-10 mx-8 bg-[#181827]">
                 {children}
             </div>
         </div>
