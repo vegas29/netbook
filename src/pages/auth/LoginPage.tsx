299ae0c3c68a577f667769e6c6f60733d0202b3a
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2024-01-28 10:15:48 -0500

    New styles for auth pages

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -13,49 +13,62 @@ export const LoginPage = () => {
 
   const handleLogin = (e) =>{
     e.preventDefault();
-    console.log('submit login');
   }
   
   return (
     <AuthLayout>
-      <h3 className="text-white mb-20 md:text-left text-3xl md:text-5xl font-bold">Sign In</h3>
+      <h2 className="text-white flex justify-center mb-14 md:text-left text-3xl md:text-5xl font-bold">Smart Library<span className="text-[#AD6BF1]">.</span></h2>
+      <h3 className="text-white mb-1 md:text-left text-2xl md:text-4xl font-bold">Login<span className="text-[#AD6BF1]">.</span></h3>
+      <p className="text-white">
+        Please enter your credentials to login to your account
+      </p>
 
       <form
         onSubmit={handleLogin}
-        className="max-w-[400px]"
+        className="max-w-[400px] mt-10"
       >
 
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          className="bg-transparent border-b border-gray-300 text-base mb-10 h-10 p-5 w-full transition duration-300 text-white"
-          autoComplete="off"
-          value={email}
-          onChange={handleInputChange}
-        />
+        <div>
+          <label className="text-white font-bold mb-3 block">Username</label>
+          <input
+            type="text"
+            placeholder="Type your email"
+            name="email"
+            className="bg-[#212133] rounded mb-5 h-10 p-6 w-full text-white focus:outline-none active:outline-none"
+            autoComplete="off"
+            value={email}
+            onChange={handleInputChange}
+          />
+        </div>
 
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          className="bg-transparent border-b border-gray-300 text-base mb-10 h-10 p-5 w-full transition duration-300 text-white"
-          value={password}
-          onChange={handleInputChange}
-        />
+        <div>
+          <label className="text-white font-bold mb-3 block">Password</label>
+          <input
+            type="password"
+            placeholder="Type your password"
+            name="password"
+            className="bg-[#212133] rounded mb-5 h-10 p-6 w-full text-white focus:outline-none active:outline-none"
+            value={password}
+            onChange={handleInputChange}
+          />
+        </div>
+
 
         <button
           type="submit"
-          className="block bg-blue-500 hover:bg-blue-600 transition duration-300 text-white w-full my-5 p-2 rounded-lg mb-12"
+          className="block bg-[#572887] hover:bg-[#4D1E7D] transition duration-300 text-white w-full my-5 p-2 rounded-lg mb-12"
         >
-          Login
+          Submit
         </button>
 
         <Link
           to="/register"
-          className="text-white"
+          className="text-white flex flex-col md:flex-row justify-center items-center"
         >
-          Don't have an account? Create new account
+          Don't have an account yet?
+          <span className="text-[#AD6BF1] hover:text-[#9e61dc]  ml-2">
+            Create new account
+          </span>
         </Link>
       </form>
     </AuthLayout>
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -18,53 +18,69 @@ export const RegisterPage = () => {
 
   return (
     <AuthLayout>
-      <h3 className="text-white mb-20 md:text-left text-3xl md:text-5xl font-bold">Sign Up</h3>
+      <h2 className="text-white flex justify-center mb-14 md:text-left text-3xl md:text-5xl font-bold">Smart Library<span className="text-[#AD6BF1]">.</span></h2>
+      <h3 className="text-white mb-1 md:text-left text-2xl md:text-4xl font-bold">Login<span className="text-[#AD6BF1]">.</span></h3>
+      <p className="text-white">
+        Please enter your credentials to login to your account
+      </p>
 
       <form
         onSubmit={handleLogin}
-        className="max-w-[400px]"
+        className="max-w-[400px] mt-10"
       >
 
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          className="bg-transparent border-b border-gray-300 text-base mb-10 h-10 p-5 w-full transition duration-300 text-white"
-          autoComplete="off"
-          value={email}
-          onChange={handleInputChange}
-        />
+        <div>
+          <label className="text-white font-bold mb-3 block">Username</label>
+          <input
+            type="text"
+            placeholder="Type your email"
+            name="email"
+            className="bg-[#212133] rounded mb-5 h-10 p-6 w-full text-white focus:outline-none active:outline-none"
+            autoComplete="off"
+            value={email}
+            onChange={handleInputChange}
+          />
+        </div>
 
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          className="bg-transparent border-b border-gray-300 text-base mb-10 h-10 p-5 w-full transition duration-300 text-white"
-          value={password}
-          onChange={handleInputChange}
-        />
+        <div>
+          <label className="text-white font-bold mb-3 block">Password</label>
+          <input
+            type="password"
+            placeholder="Type your password"
+            name="password"
+            className="bg-[#212133] rounded mb-5 h-10 p-6 w-full text-white focus:outline-none active:outline-none"
+            value={password}
+            onChange={handleInputChange}
+          />
+        </div>
 
-        <input
-          type="password"
-          placeholder="Confirm your password"
-          name="password"
-          className="bg-transparent border-b border-gray-300 text-base mb-10 h-10 p-5 w-full transition duration-300 text-white"
-          value={password}
-          onChange={handleInputChange}
-        />
-
+        <div>
+          <label className="text-white font-bold mb-3 block">Repeat password</label>
+          <input
+            type="password"
+            placeholder="Type again your password"
+            name="password"
+            className="bg-[#212133] rounded mb-5 h-10 p-6 w-full text-white focus:outline-none active:outline-none"
+            value={password}
+            onChange={handleInputChange}
+          />
+        </div>
+        
         <button
           type="submit"
-          className="block bg-blue-500 hover:bg-blue-600 transition duration-300 text-white w-full my-5 p-2 rounded-lg mb-12"
+          className="block bg-[#572887] hover:bg-[#4D1E7D] transition duration-300 text-white w-full my-5 p-2 rounded-lg mb-12"
         >
-          Login
+          Submit
         </button>
 
         <Link
           to="/"
-          className="text-white"
+          className="text-white flex flex-col md:flex-row justify-center items-center"
         >
-          Do you have already account? Sign In
+          Don't have already an account?
+          <span className="text-[#AD6BF1] hover:text-[#9e61dc]  ml-2">
+            Sign In
+          </span>
         </Link>
       </form>
     </AuthLayout>
