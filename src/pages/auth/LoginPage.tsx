5d7c2dd733cd7e6e29f16fc6423caa19f779a8fb
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2024-01-28 15:38:08 -0500

    Fix: Comment useEffect on LoginPage

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthLayout } from "../../layouts/AuthLayout";
 import { useAuthStore, useForm } from "../../hooks/";
