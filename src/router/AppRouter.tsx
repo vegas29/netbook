619917e4db7ccbaf1eef6eddb804eaac324ba1da
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2024-02-02 02:11:09 -0500

    Fix: Remove useState

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useAuthStore } from "../hooks";
 import { LoginPage } from '../pages/auth/LoginPage';
